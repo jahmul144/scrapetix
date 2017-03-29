f2ea5c37f37ea2d162616d4a078ffb3e5b136254
jahmul144 <erikhummel14@gmail.com>
2017-03-29 22:29:42 +0000

    First Test Selenium

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,1 +1,24 @@
-#First Test of Selenium
+#First Test of Seleniumi
+import unittest
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+class SearchTest(unittest.TestCase):
+    def setUp(self):
+        # create a new Chrome session
+        self.display = Display(visible=0, size=(800, 600))
+        self.display.start()
+        self.driver = webdriver.Chrome(executable_path="/home/ubuntu/Downloads/chromedriver")
+        
+    
+    def test_titleload(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        
+    def tearDown(self):
+        self.driver.quit()
+        self.display.stop()
+
+if __name__ == "__main__":
+    unittest.main()
